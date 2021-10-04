646fc4ab490977bd7a56c92dae7aed9f13dbd7a0
hulk211 <88299365+hulk211@users.noreply.github.com>
2021-10-03 23:08:59 -0600

    Add files via upload

---
+++ cue.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="bon2.css">
+</head>
+<body>
+<header>
+	<a href="reglasort.php"><img src="logo.png" alt="logo" height="200"  /></a>
+	
+<h1 style="text-align: center;">Cuento</h1>
+</header>
+<nav>
+<h1 align="center">Primer cuento</h1>	
+<p>
+<img src="c.png" alt="logo"  />
+</p>
+<a href="com.php"><button type="input">Comprobación</button></a>
+
+</nav>
+</body>
+
+</html>
