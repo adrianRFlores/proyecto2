64b0f0bc4f619c782231e617cd52ef7b33950960
hulk211 <88299365+hulk211@users.noreply.github.com>
2021-10-27 22:54:29 -0600

    Add files via upload

---
+++ comp4.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="bon2.css">
+</head>
+<body>
+<header>
+	<a href="reglasort.php"><img src="3c.png" alt="logo" height="200"  /></a>
+	
+<h1 style="text-align: center;">Comprobación lectora</h1>
+</header>
+<nav>
+<h1 align="center">Responder:</h1>	
+<p> 
+<p>1. ¿Cuantos cerditos habian?</p>
+<p><a href="#">A. 1</a>
+<a href="#">B. 2</a>
+<a href="#">C. 3</a>
+<a href="#">D. 4 </a>
+</p>
+<p>
+<p>2. ¿La casa de ladrillo fue destruida?</p>
+<p><a href="#">A. Verdadero</a>
+<a href="#">B. Falso</a>
+</p>
+<p>
+
+<a href="#"><button type="input">Revisar</button></a>
+
+</nav>
+
+</body>
+
+</html>
