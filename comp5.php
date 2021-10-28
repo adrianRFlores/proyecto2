87f057b3bd9bd97c4a0d136956e2db65d8d70d9c
hulk211 <88299365+hulk211@users.noreply.github.com>
2021-10-27 23:35:14 -0600

    Add files via upload

---
+++ comp5.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="bon2.css">
+</head>
+<body>
+<header>
+	<a href="reglasort.php"><img src="pf.png" alt="logo" height="200"  /></a>
+	
+<h1 style="text-align: center;">Comprobación lectora</h1>
+</header>
+<nav>
+<h1 align="center">Responder:</h1>	
+<p> 
+<p>1. ¿Qué característica es propia del patito?</p>
+<p><a href="#">A. Su belleza</a>
+<a href="#">B. Su fealdad</a>
+<a href="#">C. Su inteligencia</a>
+<a href="#">D. Su humildad </a>
+</p>
+<p>
+<p>2. ¿Por qué el patito era diferente a los otros?</p>
+<p><a href="#">A. Su personalidad</a>
+<a href="#">B. Mutaciones.</a>
+<a href="#">C. Era un cisne</a>
+
+</p>
+
+<a href="#"><button type="input">Revisar</button></a>
+
+</nav>
+
+</body>
+
+</html>
---
+++ cue5.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="bon2.css">
+</head>
+<body>
+<header>
+	<a href="reglasort.php"><img src="pf.png" alt="logo" height="200"  /></a>
+	
+<h1 style="text-align: center;">Cuento</h1>
+</header>
+<nav>
+<h1 align="center">Patito feo</h1>	
+<p>
+En la granja había un gran alboroto: los polluelos de Mamá Pata estaban rompiendo el cascarón.
+
+Uno a uno, comenzaron a salir. Mamá Pata estaba tan emocionada con sus adorables patitos que no notó que uno de sus huevos, el más grande de todos, permanecía intacto.
+</p>
+<p>
+A las pocas horas, el último huevo comenzó a romperse. Mamá Pata, todos los polluelos y los animales de la granja, se encontraban a la expectativa de conocer al pequeño que tardaba en nacer. De repente, del cascarón salió un patito muy alegre. Cuando todos lo vieron se quedaron sorprendidos, este patito no era pequeño ni amarillo y tampoco estaba cubierto de suaves plumas. Este patito era grande, gris y en vez del esperado graznido, cada vez que hablaba sonaba como una corneta vieja.
+
+Aunque nadie dijo nada, todos pensaron lo mismo: “Este patito es demasiado feo”.
+
+Pasaron los días y todos los animales de la granja se burlaban de él.
+</p>
+<p>
+Finalmente, llegó la primavera. El patito feo vio a una familia de cisnes nadando en el estanque y quiso acercárseles. Pero recordó cómo todos se burlaban de él y agachó la cabeza avergonzado. Cuando miró su reflejo en el agua se quedó asombrado. Él no era un patito feo, sino un apuesto y joven cisne. Ahora sabía por qué se veía tan diferente a sus hermanos y hermanas. ¡Ellos eran patitos, pero él era un cisne! Feliz, nadó hacia su familia.
+</p>
+
+<a href="comp5.php"><button type="input">Comprobación</button></a>
+
+</nav>
+
+
+</body>
+
+</html>
