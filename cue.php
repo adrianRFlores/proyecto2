392a1151737593e2486cc2ac1b0ede966d920e62
Gohan1233 <gar21352@uvg.edu.gt>
2021-11-17 22:33:21 -0600

    Modificacion de cuentos
    Se coloco un fondo mas bonito, con el menu principal, y la forma de que la lectura sea mejor

--- cue.php
+++ cue.php
@@ -1,16 +1,53 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
-	<link rel="stylesheet" href="bon2.css">
+	<title>La invitación de la señora araña</title>
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 </head>
-<body>
-<header>
-	<a href="reglasort.php"><img src="ar.png" alt="logo" height="200"  /></a>
-	
-<h1 style="text-align: center;">Cuento</h1>
-</header>
-<nav>
+<body class="img" style="background-image: url(images/cuentos.jpg);">
+	<nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color: #000000;">
+      	<div class="container">
+	        <a class="navbar-brand" href="MenuUsuario.php">
+	          	<img src="images/logo.png" alt="" width="70" height="60" >
+	        </a>
+	        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	        	<span class="navbar-toggler-icon"></span>
+	        </button>     
+	        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	          	<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+	        		<li class="nav-item">
+	          			<a class="nav-link active" aria-current="page" href="MenuUsuario.php">Inicio</a>
+	        		</li>
+	            	<li class="nav-item">
+	              		<a class="nav-link" href="Nosotros.php">¿Quienes somos?</a>
+	            	</li>
+	            	<li class="nav-item">
+	              		<a class="nav-link" href="MenuPronunciacion.php">Pronunciación</a>
+	            	</li>
+	            	<li class="nav-item dropdown">
+	              		<a class="nav-link" href="reglasort.php">Reglas</a>
+	            	</li>
+	            	<li class="nav-item">
+	              		<a class="nav-link" href="MenuLeer.php">Lecturas</a>
+	            	</li>
+	          	</ul>
+	            <form action="CerrarSesion.php" method="POST">
+	            	<button class="btn btn-outline-success" type="submit">Cerrar Sesión</button>
+	            </form>
+    		</div>
+      	</div>
+    </nav>
+<nav style="position: absolute;
+	margin: 270px auto 0;
+	width: 700px;
+	background: #191919;
+	transform: translate(-50%,-50%);
+	padding: 40px;
+	top: 30%;
+	left: 50%;
+	text-align: center;
+	color: white;">
 <h1 align="center">La invitación de la señora araña</h1>	
 <p>
 Un día, la señora araña decidió hacer una fiesta. 
