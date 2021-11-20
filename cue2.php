1ec081f7830b28ebd329518c9db4052f1406c200
hulk211 <88299365+hulk211@users.noreply.github.com>
2021-11-20 12:56:01 -0600

    Add files via upload

--- cue2.php
+++ cue2.php
@@ -1,18 +1,54 @@
-<!DOCTYPE html>
+ <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
-	<link rel="stylesheet" href="bon2.css">
+	<title>La invitación de la señora araña</title>
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 </head>
-<body>
-<header>
-	<a href="reglasort.php"><img src="cap.png" alt="logo" height="200"  /></a>
-	
-<h1 style="text-align: center;">Cuento</h1>
-</header>
-<nav>
-<h1 align="center">La caperucita roja</h1>	
-
+<body class="img" style="background-image: url(images/cuentos.jpg);">
+	<nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color: #000000;">
+      	<div class="container">
+	        <a class="navbar-brand" href="MenuUsuario.php">
+	          	<img src="images/logo.png" alt="" width="70" height="60" >
+	        </a>
+	        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	        	<span class="navbar-toggler-icon"></span>
+	        </button>     
+	        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	          	<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+	        		<li class="nav-item">
+	          			<a class="nav-link active" aria-current="page" href="MenuUsuario.php">Inicio</a>
+	        		</li>
+	            	<li class="nav-item">
+	              		<a class="nav-link" href="Nosotros.php">¿Quienes somos?</a>
+	            	</li>
+	            	<li class="nav-item">
+	              		<a class="nav-link" href="MenuPronunciacion.php">Pronunciación</a>
+	            	</li>
+	            	<li class="nav-item dropdown">
+	              		<a class="nav-link" href="reglasort.php">Reglas</a>
+	            	</li>
+	            	<li class="nav-item">
+	              		<a class="nav-link" href="MenuLeer.php">Lecturas</a>
+	            	</li>
+	          	</ul>
+	            <form action="CerrarSesion.php" method="POST">
+	            	<button class="btn btn-outline-success" type="submit">Cerrar Sesión</button>
+	            </form>
+    		</div>
+      	</div>
+    </nav>
+<nav style="position: absolute;
+	margin: 270px auto 0;
+	width: 700px;
+	background: #191919;
+	transform: translate(-50%,-50%);
+	padding: 40px;
+	top: 30%;
+	left: 50%;
+	text-align: center;
+	color: white;">
+<h1 align="center">Caperucita roja</h1>	
 <p>
 	Había una vez una dulce niña que quería mucho a su madre y a su abuela. Les ayudaba en todo lo que podía y como era tan buena el día de su cumpleaños su abuela le regaló una caperuza roja. Como le gustaba tanto e iba con ella a todas partes, pronto todos empezaron a llamarla Caperucita roja.
 
@@ -32,8 +68,11 @@ El lobo mandó a Caperucita por el camino más largo y llegó antes que ella a c
 En cuanto dijo esto el lobo se lanzó sobre Caperucita y se la comió también. Su estómago estaba tan lleno que el lobo se quedó dormido.
 </p>
 <p></p>
-<a href="com2.php"><button type="input">Comprobación</button></a>
 
+<a href="com.php">
+	<button type="input" class="form-control btn btn-primary submit px-3" name="btn" >Comprobación</button>
+
+</a>
 </nav>
 
 
