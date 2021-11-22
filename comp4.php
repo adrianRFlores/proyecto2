01e2cc4b1ed10e487bc154e701c316de0f55f90e
hulk211 <88299365+hulk211@users.noreply.github.com>
2021-11-21 22:15:19 -0600

    Add files via upload

--- comp4.php
+++ comp4.php
@@ -1,35 +1,75 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
-	<link rel="stylesheet" href="bon2.css">
+	<title>Comprobación lectora los 3 cerditos</title>
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 </head>
-<body>
-<header>
-	<a href="reglasort.php"><img src="3c.png" alt="logo" height="200"  /></a>
+<body class="img" style="background-image: url(images/cuentos.jpg);">
 	
-<h1 style="text-align: center;">Comprobación lectora</h1>
-</header>
-<nav>
-<h1 align="center">Responder:</h1>	
-<p> 
-<p>1. ¿Cuantos cerditos habian?</p>
-<p><a href="#">A. 1</a>
-<a href="#">B. 2</a>
-<a href="#">C. 3</a>
-<a href="#">D. 4 </a>
-</p>
-<p>
-<p>2. ¿La casa de ladrillo fue destruida?</p>
-<p><a href="#">A. Verdadero</a>
-<a href="#">B. Falso</a>
-</p>
-<p>
+	<nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color: #000000;">
+      	<div class="container">
+	        <a class="navbar-brand" href="MenuUsuario.php">
+	          	<img src="images/logo.png" alt="" width="70" height="60" >
+	        </a>
+	        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	        	<span class="navbar-toggler-icon"></span>
+	        </button>     
+	        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	          	<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+	        		<li class="nav-item">
+	          			<a class="nav-link active" aria-current="page" href="MenuUsuario.php">Inicio</a>
+	        		</li>
+	            	<li class="nav-item">
+	              		<a class="nav-link" href="Nosotros.php">¿Quienes somos?</a>
+	            	</li>
+	            	<li class="nav-item">
+	              		<a class="nav-link" href="MenuPronunciacion.php">Pronunciación</a>
+	            	</li>
+	            	<li class="nav-item dropdown">
+	              		<a class="nav-link" href="reglasort.php">Reglas</a>
+	            	</li>
+	            	<li class="nav-item">
+	              		<a class="nav-link" href="MenuLeer.php">Lecturas</a>
+	            	</li>
+	          	</ul>
+	            <form action="CerrarSesion.php" method="POST">
+	            	<button class="btn btn-outline-success" type="submit">Cerrar Sesión</button>
+	            </form>
+    		</div>
+      	</div>
+    </nav>
+<nav style="position: absolute;
+	margin: 300px auto 0;
+	width: 700px;
+	background: #191919;
+	transform: translate(-50%,-50%);
+	padding: 40px;
+	top: 30%;
+	left: 50%;
+	text-align: left;
+	color: white;">
+<h1 align="center">Responda las siguientes preguntas:</h1>
+<form action="pr.php" method="POST">
+<p>1. ¿Cuantos cerditos eran?</p>
+<input style = "width:5%" type="radio" id="cb1" name="cb1" value="0"><label>A. 1</label><br>
+<input style = "width:5%" type="radio" id="cb1" name="cb1" value="0"><label>B. 2</label><br>
+<input style = "width:5%" type="radio" id="cb1" name="cb1" value="1"><label>C. 3</label><br>
 
-<a href="#"><button type="input">Revisar</button></a>
 
+
+<p>2. ¿La casa de paja fue destruida ?</p>
+<input style = "width:5%" type="radio" id="cb2" name="cb2" value="1"><label>A. Verdadero.</label><br>
+<input style = "width:5%" type="radio" id="cb2" name="cb2" value="0"><label>B. Falso.</label><br>
+
+
+
+<p>3. ¿La casa de ladrillo fue destruida? </p>
+<input style = "width:5%" type="radio" id="cb3" name="cb3" value="0"><label>A. Verdadero.</label><br>
+<input style = "width:5%" type="radio" id="cb3" name="cb3" value="1"><label>B. Falso.</label><br>
+
+<a href="#"><button type="input" class="form-control btn btn-primary submit px-3" name="btn">Revisar</button></a>
+ </form>
 </nav>
-
 </body>
-
 </html>
