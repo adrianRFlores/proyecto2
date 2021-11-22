4f6986be8711fe4bfba720de4aa6e9d52fe84159
Gohan1233 <gar21352@uvg.edu.gt>
2021-11-22 01:25:17 -0600

    Ingresar maestros

--- Administrador.php
+++ Administrador.php
@@ -4,14 +4,13 @@
   <title>Ingresar Maestro</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body class="img" style="background-image: url(images/bg.jpg);">
 	<nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color: #000000;">
       	<div class="container">
-		      <a class="navbar-brand" href="MenuUsuario.php">
+		      <a class="navbar-brand" href="Administrador.php">
 		          	<img src="images/logo.png" alt="" width="70" height="60" >
 		      </a>
 		      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -20,16 +19,22 @@
 		      <div class="collapse navbar-collapse" id="navbarSupportedContent">
 		          	<ul class="navbar-nav me-auto mb-2 mb-lg-0">
 		        		<li class="nav-item">
-		          			<a class="nav-link active" aria-current="page" href="MenuUsuario.php">Inicio</a>
+		          			<a class="nav-link active" aria-current="page" href="MenuAdmin.php">Inicio</a>
 		        		</li>
 		            	<li class="nav-item">
-		              	<a class="nav-link" href="Administrador.php">Ingresar Maestro</a>
+		              		<a class="nav-link" href="Administrador.php">Crear Maestro</a>
 		            	</li>
 		            	<li class="nav-item">
-		              	<a class="nav-link" href="Instituto.php">Ingresar Instituto</a>
+		              		<a class="nav-link" href="Institutos.php">Pronunciación</a>
+		            	</li>
+		            	<li class="nav-item dropdown">
+		              		<a class="nav-link" href="#"></a>
+		            	</li>
+		            	<li class="nav-item">
+		              		<a class="nav-link" href="#"></a>
 		            	</li>
 		          	</ul>
-		            <form action="CerrarSesion.php" method="POST">
+		            <form style="align: right;" action="CerrarSesion.php" method="POST">
 		            	<button class="btn btn-outline-success" type="submit">Cerrar Sesión</button>
 		            </form>
 	    		</div>
@@ -38,19 +43,11 @@
 	<form action="GuardarMaestro.php" method="POST" class="signin-form">
 		<section class="ftco-no-pt">
 			<div align="center">
-				<table>
-					<tr>
-						<td>
-							<a href="Login.php">
-								<img src="Images/Logo.png" style="border-radius: 15px;"  height="100"/>
-							</a>
-						</td>
-					</tr>
-				</table>
+				<br><br><br><br><br>
 				<div class="row justify-content-center">
 					<div class="col-md-6 col-lg-4">
 						<div class="login-wrap p-0">
-			      	<h3 class="mb-4 text-center">Crear Cuenta</h3>
+			      	<h3 class="mb-4 text-center">Ingresar Maestro</h3>
 		      		<div class="form-group">
 		      			<input type="text"  class="form-control" placeholder="Ingrese el nombre del catedratico" name="text1" required>
 		      		</div>
@@ -84,12 +81,9 @@
 	              <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
 	            </div>
 	            <div class="form-group">
-	            	<button type="submit" class="form-control btn btn-primary submit px-3" name="btn">Ingresar Catedratico</button>
+	            	<button type="submit" class="form-control btn btn-primary submit px-3" name="btn">Guardar Maestro</button>
 	            </div>
-								<div class="text-md-center">
-									<a href="Login.php" style="color: #fff">Regresar
-								</div>
-	            </div>
+	           </div>
 			      </div>
 					</div>
 				</div>
