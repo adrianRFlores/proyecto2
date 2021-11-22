8e4a37371527ddb3deb45fc69e68debeb8437d98
Gohan1233 <gar21352@uvg.edu.gt>
2021-11-21 23:44:33 -0600

    Registrar datos

--- Registro.php
+++ Registro.php
@@ -1,48 +1,68 @@
 <!doctype html>
 <html lang="en">
-  <head>
-  	<title>Registro</title>
+<head>
+  <title>Registro</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 		<link rel="stylesheet" href="css/style.css">
-	</head>
-	<body class="img" style="background-image: url(images/bg.jpg);">
-	<section class="ftco-no-pt">
-		<div align="center">
-			<table>
-				<tr>
-					<td>
-						<a href="Registro.php">
-							<img src="Images/Logo.png" style="border-radius: 15px;"  height="100"/>
-						</a>
-					</td>
-				</tr>
-			</table>
-			<div class="row justify-content-center">
-				<div class="col-md-6 col-lg-4">
-					<div class="login-wrap p-0">
-		      	<h3 class="mb-4 text-center">Crear Cuenta</h3>
-		      	<form action="Registro.php" class="signin-form">
+</head>
+<body class="img" style="background-image: url(images/bg.jpg);">
+	<form action="GuardarAlumno.php" method="POST" class="signin-form">
+		<section class="ftco-no-pt">
+			<div align="center">
+				<table>
+					<tr>
+						<td>
+							<a href="Login.php">
+								<img src="Images/Logo.png" style="border-radius: 15px;"  height="100"/>
+							</a>
+						</td>
+					</tr>
+				</table>
+				<div class="row justify-content-center">
+					<div class="col-md-6 col-lg-4">
+						<div class="login-wrap p-0">
+			      	<h3 class="mb-4 text-center">Crear Cuenta</h3>
 		      		<div class="form-group">
 		      			<input type="text"  class="form-control" placeholder="Ingrese su Nombre Completo" name="text1" required>
 		      		</div>
 		      		<div class="form-group">
 		      			<input type="text"  class="form-control" placeholder="Ingrese el Codigo" name="text2" required>
 		      		</div>
-		      		<div class="form-group">
-		      			<input type="text"  class="form-control" placeholder="Ingrese el Nombre de su Instituto" name="text3" required>
-		      		</div>
-		      		<div class="form-group">
-		      			<input type="text"  class="form-control" placeholder="Ingrese el Codigo de su Tutor" name="text4" required>
-		      		</div>
+		      		<div class="form-group">	
+			     			<select name="selec1" class="form-control">
+									<option value="" class="form-control" style=" color: #000 !important;">Catedraticos...</option>
+									<?php
+										session_start();
+										ob_start();
+										$instituto = $_SESSION['instituto'];
+										/*echo "<script>
+												alert('".$instituto."');
+											</script>";*/
+										$usuario = "root";
+										$clave = "";
+										$servidor = "localhost";
+										$base = "aprende";
+
+							  			$cone = mysqli_connect($servidor, $usuario, $clave, $base);
+							  			
+							  			$query = "SELECT CodigoMaestro, Nombre FROM maestro WHERE CodigoInstituto = '$instituto'";
+							  			$q = mysqli_query($cone, $query);
+					  					
+							 				while ($x = mysqli_fetch_array($q)){
+							  				echo "<option style='color: black;' value='".$x['CodigoMaestro']."'>".$x['Nombre']."</option>";
+							 				}
+										?>
+								</select>
+							</div>
 	            <div class="form-group">
-	              <input id="password-field" type="password" class="form-control" placeholder="Ingrese una Contraseña" name="text5" required>
+	              <input id="password-field" type="password" class="form-control" placeholder="Ingrese una Contraseña" name="text4" required>
 	              <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
 	            </div>
 	            <div class="form-group">
-	              <input id="password-field2" type="password" class="form-control" placeholder="Confirmar Contraseña" name="text6" required>
+	              <input id="password-field2" type="password" class="form-control" placeholder="Confirmar Contraseña" name="text5" required>
 	              <span toggle="#password-field2" class="fa fa-fw fa-eye field-icon toggle-password"></span>
 	            </div>
 	            <div class="form-group">
@@ -52,82 +72,12 @@
 									<a href="Login.php" style="color: #fff">Regresar
 								</div>
 	            </div>
-	          </form>
-		      </div>
+			      </div>
+					</div>
 				</div>
 			</div>
-		</div>
-	</section>
-	<?php
-
-		$usuario = "root";
-  	$clave = "";
-  	$servidor = "localhost";
-  	$base = "aprende";
-  	$cone = mysqli_connect($servidor, $usuario, $clave, $base);
-  	if (isset($_POST['btn']))
-		{	
-			$nombre = $_POST['tex1'];
-			$codigo = $_POST['tex2'];
-			$instituto = $_POST['tex3'];
-			$codtuto = $_POST['tex4'];
-			$contra = $_POST['tex5'];
-			$contra2 = $_POST['tex6'];
-			$tipo = "Alumno";
-			if(!$cone)
-		   {
-		     die ("No se pudo establecer la Conexión");
-		   }
-		   else
-	    {
-	    	if($nombre != "" && $codigo != "" && $instituto != "" && $codtuto != "" && $contra != "" && $contra2)
-	    	{
-		   		$cons= "SELECT CodigoUsuario from usuario";
-		   		$x1 = mysqli_query($cone, $cons);
-		    	$bandera = false;
-		   		while (($a = mysqli_fetch_array($x1)) && !$bandera)
-					{
-						if ($a['CodigoUsuario'] == $codigo) $bandera = true;
-					}
-					if ($bandera)
-					{
-						echo "<script>
-						alert('El Codigo es repetido');
-						window.location.replace('Registro.php');
-						</script>";
-					}
-					else
-					{
-						if ($contra == $contra2) {
-							$usu = "INSERT INTO usuario values('$Tipo', '$codigo', '$contra', '$codtuto')";
-			    		$x2 = mysqli_query($cone, $usu);
-			    		$alu = "INSERT INTO alumno values('$codigo', '$nombre', '$codtuto', 0.00)";
-			    		$x3 = mysqli_query($cone, $alu);
-			    		if ($x2 && $x3)
-			    		{
-				   			echo "<script>
-								alert('Se ha guardado con exito los datos');
-								</script>";
-							}
-						}
-						else
-						{
-							echo "<script>
-							alert('La contraseña es distinta');
-							window.location.replace('Registro.php');
-							</script>";
-						}
-					}
-				}
-				else
-				{
-					echo "<script>
-					alert('Llene todos los campos');
-					</script>";
-				}
-	    }  
-		}
-	?>
+		</section>
+	</form>
 	<script src="js/jquery.min.js"></script>
   <script src="js/popper.js"></script>
   <script src="js/bootstrap.min.js"></script>
